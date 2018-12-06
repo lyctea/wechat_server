4366db2f8f03d57df11f4dea4e330a857ca124c1
lyctea <396136337@qq.com>
2018-12-06 15:06:24 +0800

    feat: 微信上传临时素材

--- server/middlewares/router.js
+++ server/middlewares/router.js
@@ -2,11 +2,24 @@ import Router from 'koa-router'
 import config from '../config'
 import reply from '../wechat/reply'
 import wechatMiddle from '../wechat-lib/middleware'
+import { resolve } from 'path'
 
 export const router = app => {
   const router = new Router()
 
   router.all('/wechat-hear', wechatMiddle(config.wechat, reply))
+
+  router.get('/upload', (ctx, next) => {
+    let mp = require('../wechat')
+    let client = mp.getWechat()
+
+    // 调用 uploadMaterial， 并传递参数
+    client.handle(
+      'uploadMaterial',
+      'video',
+      resolve(__dirname, '../../ice.mp4')
+    )
+  })
 
   app.use(router.routes()).use(router.allowedMethods())
 }
--- server/wechat-lib/index.js
+++ server/wechat-lib/index.js
@@ -1,8 +1,39 @@
 import request from 'request-promise'
+// import formstream from 'formsrteam'
+import fs from 'fs'
+import path from 'path'
 
-const base = 'https://api.weixin.qq.com/cgi-bin'
+const base = 'https://api.weixin.qq.com/cgi-bin/'
 const api = {
-  accessToken: base + '/token?grant_type=client_credential'
+  accessToken: base + 'token?grant_type=client_credential',
+  temporary: {
+    upload: base + 'media/upload?',
+    fetch: base + 'media/get?'
+  },
+  permanent: {
+    upload: base + 'material/add_material?',
+    uploadNews: base + 'media/add_news?',
+    uploadNewsPick: base + 'media/uploadimg?',
+    fetch: base + 'media/get_material',
+    del: base + 'media/del_material',
+    update: base + 'media/update_news',
+    count: base + 'media/get_materialcount',
+    batch: base + 'media/batchget_material'
+  }
+}
+
+/**
+ * 读取文件大小
+ * @param filepath 文件路径
+ * @returns {Promise<any>}
+ */
+function statFile(filepath) {
+  return new Promise((resolve, reject) => {
+    fs.stat(filepath, (err, stat) => {
+      if (err) reject(err)
+      else resolve(stat)
+    })
+  })
 }
 
 export default class Wechat {
@@ -16,16 +47,20 @@ export default class Wechat {
     this.fetchAccessToken()
   }
 
+  /**
+   * 请求微信接口
+   * @param options
+   * @returns {Promise<void>}
+   */
   async request(options) {
     options = Object.assign({}, options, { json: true })
     try {
       const response = await request(options)
+
       return response
     } catch (error) {
       console.error(error)
     }
-
-    return response
   }
 
   async fetchAccessToken() {
@@ -61,5 +96,70 @@ export default class Wechat {
 
     //  判断是否过期
     return now < expiresIn
+  }
+
+  /**
+   * 钩子方法，调用不同方法，获取options，调用微信接口
+   * @param operation
+   * @param args
+   * @returns {Promise<void>}
+   */
+  async handle(operation, ...args) {
+    const tokenData = await this.fetchAccessToken()
+    const options = await this[operation](tokenData.access_token, ...args)
+    const data = await this.request(options)
+
+    return data
+  }
+
+  /**
+   * 配置微信素材上传参数
+   * @param token 用户凭证
+   * @param type 素材类型
+   * @param material 原材料
+   * @param permanent 标记是否是永久素材
+   */
+  async uploadMaterial(token, type, material, permanent) {
+    let form = {}
+    let url = api.temporary.upload // 临时文件api
+
+    if (permanent) {
+      url = api.permanent.uploadNewsPic
+
+      _.extend(form, permanent)
+    }
+
+    if (type === 'news') {
+      url = api.permanent.uploadNews
+      form = material
+    } else {
+      // form = formstream()
+      // const stat = await statFile(material)
+      // form.file('media', material, path.basename(material), stat.size)
+      form.media = fs.createReadStream(material)
+    }
+
+    let uploadUrl = url + 'access_token=' + token
+
+    if (!permanent) {
+      uploadUrl += '&type=' + type
+    } else {
+      // form.field('access_token', access_token)
+      form.access_token = access_token
+    }
+
+    const options = {
+      method: 'POST',
+      url: uploadUrl,
+      json: true
+    }
+
+    if (type === 'news') {
+      options.body = form
+    } else {
+      options.formData = form
+    }
+
+    return options
   }
 }
